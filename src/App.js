624d986a851ed2d77af1a4cfb95e7db8bd906ab4
Brett Seifried <seifriedbrett@gmail.com>
2021-12-08 14:14:08 -0800

    Controlsm PokeList, pokemon.js, app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { getPokemon } from './services/pokemon';
+import { useEffect, useState } from 'react';
+import PokeList from './Components/PokeList/PokeList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [pokemon, setPokemon] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getPokemon(query)
+      setPokemon(data.results);
+      setTimeout(() => {
+        setLoading(false);
+      } 2000);
+    })
+    };
+    fetchData();
+  }, [loading]);
+  return () => {
+    <div className="app">
+      <PokeList pokemon={pokemon}/>
+    </div>;
+  };
 }
 
 export default App;
