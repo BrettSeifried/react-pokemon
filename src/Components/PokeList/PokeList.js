44d89482998fee1e23c7d02890a79d3567b94442
Brett Seifried <seifriedbrett@gmail.com>
2021-12-08 15:58:47 -0800

    added images

--- src/Components/PokeList/PokeList.js
+++ src/Components/PokeList/PokeList.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
+// function filterPokemon() {
+//   return pokemon.filter((obj => {
+//     return obj.pokemon.cinludes(query) && (obj.)
+//   }))
+// }
+
 export default function PokeList({ pokemon }) {
   return (
     <div>
       {pokemon.map((poke) => (
         <p key={poke.id}>
           {poke.pokemon}
-          {/* <img
-            alt={poke.pokemon}
-            src={`http://assets.pokemon.com/assets/cms2/img/pokedex/full/${poke.species_id}.png`}
-          ></img> */}
+          <img alt={poke.pokemon} src={poke.url_image}></img>
         </p>
       ))}
       ;
