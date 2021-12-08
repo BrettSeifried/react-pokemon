158cf3d84555fa07bf2e7a2cbdff0713051c7ff3
Brett Seifried <seifriedbrett@gmail.com>
2021-12-08 15:45:17 -0800

    started work on images showing up

--- src/Components/PokeList/PokeList.js
+++ src/Components/PokeList/PokeList.js
@@ -4,7 +4,13 @@ export default function PokeList({ pokemon }) {
   return (
     <div>
       {pokemon.map((poke) => (
-        <p key={poke.id}>{poke.pokemon}</p>
+        <p key={poke.id}>
+          {poke.pokemon}
+          {/* <img
+            alt={poke.pokemon}
+            src={`http://assets.pokemon.com/assets/cms2/img/pokedex/full/${poke.species_id}.png`}
+          ></img> */}
+        </p>
       ))}
       ;
     </div>
